2a49fc0f9914bb0f8bbf3c8cb9c3d9e1386fe172
MuhammadRifal22 <rifalmuhammad797@gmail.com>
2020-11-24 15:19:31 +0700

    Menambahkan Fitur Filter, Serach Data, Paging Page dan Sort Data

--- RentalKendaraan_20180140121/Controllers/KendaraansController.cs
+++ RentalKendaraan_20180140121/Controllers/KendaraansController.cs
@@ -18,11 +18,78 @@ namespace RentalKendaraan_20180140121.Controllers
             _context = context;
         }
 
-        // GET: Kendaraans
-        public async Task<IActionResult> Index()
+
+        
+        public async Task<IActionResult> Index(string ktsd, string searchString, string sortOrder, string currentFilter, int? pageNumber)
+        
+        
         {
-            var rentalKendaraanContext = _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation);
-            return View(await rentalKendaraanContext.ToListAsync());
+            //var rentalKendaraanContext = _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation);
+            //return View(await rentalKendaraanContext.ToListAsync());
+
+            //buat list menyimpan ketersediaan
+            var ktsdList = new List<string>();
+            //query mengambil data
+            var ktsdQuery = from d in _context.Kendaraan orderby d.Ketersediaan select d.Ketersediaan;
+
+            ktsdList.AddRange(ktsdQuery.Distinct());
+
+            //untuk menampilkan di view
+            ViewBag.ktsd = new SelectList(ktsdList);
+
+            //panggil db context
+            var menu = from m in _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation) select m;
+
+            //untuk memilih dropdownlist ketersediaan
+            if (!string.IsNullOrEmpty(ktsd))
+            {
+                menu = menu.Where(x => x.Ketersediaan == ktsd);
+            }
+
+            //untuk search data
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                menu = menu.Where(s => s.NoPolisi.Contains(searchString) || s.NamaKendaraan.Contains(searchString) || s.NoStnk.Contains(searchString));
+            }
+
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    menu = menu.OrderByDescending(s => s.NamaKendaraan);
+                    break;
+                case "Date":
+                    menu = menu.OrderBy(s => s.Ketersediaan);
+                    break;
+                case "date_desc":
+                    menu = menu.OrderByDescending(s => s.Ketersediaan);
+                    break;
+                default:
+                    menu = menu.OrderBy(s => s.NamaKendaraan);
+                    break;
+            }
+
+            //membuat pagedList
+            ViewData["CurrentSort"] = sortOrder;
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            //definisi jumlah data pada halaman
+            int pageSize = 5;
+
+            return View(await PaginatedList<Kendaraan>.CreateAsync(menu.AsNoTracking(), pageNumber ?? 1, pageSize));
+            //var rentKendaraanContext = _context.Kendaraan.Include(k => k.IdJenisKendaraanNavigation);
+            //return View(await rentKendaraanContext.ToListAsync());
         }
 
         // GET: Kendaraans/Details/5
--- RentalKendaraan_20180140121/Models/Kendaraan.cs
+++ RentalKendaraan_20180140121/Models/Kendaraan.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_20180140121.Models
 {
@@ -11,12 +13,27 @@ namespace RentalKendaraan_20180140121.Models
         }
 
         public int IdKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Nama Kendaraan tidak boleh kosong!!")]
         public string NamaKendaraan { get; set; }
+
+        [Required(ErrorMessage = "No Polisi tidak boleh kosong!!")]
         public string NoPolisi { get; set; }
+
+
+        //[RegularExpression("^[0-9]^$", ErrorMessage = "No STNK hanya boleh di isi dengan angka")]
+        [MinLength(6, ErrorMessage = "No STNK minimal 6 angka")]
+        [MaxLength(8, ErrorMessage = "No STNK maksimal 8 angka")]
+        [Required(ErrorMessage = "No STNK tidak boleh kosong!!")]
         public string NoStnk { get; set; }
+
         public int? IdJenisKendaraan { get; set; }
+
+        [Required(ErrorMessage = "Ketersedian tidak boleh kosong!!")]
+        //[RegularExpression("^[0-9]^$", ErrorMessage = "No STNK hanya boleh di isi dengan angka")]
         public string Ketersediaan { get; set; }
 
+        [DisplayName("JenisKendaraan")]
         public JenisKendaraan IdJenisKendaraanNavigation { get; set; }
         public ICollection<Peminjaman> Peminjaman { get; set; }
     }
